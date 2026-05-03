392ca7d8dd27b70063f983f86b49f87b3c2f92e7
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2026-05-03 14:31:35 +0700

    register CartsModule with TypeORM entities and controller services

--- back-end/src/carts/carts.module.ts
+++ back-end/src/carts/carts.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartDetail } from './entities/cart_detail';
 import { Cart } from './entities/cart.entity';
 import { Coupon } from 'src/coupon/entities/coupon.entity';
-import { User } from 'discord.js';
+import { User } from 'src/users/entities/user.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart, CartDetail, Coupon, User])],
+  imports: [TypeOrmModule.forFeature([Cart, CartDetail, Coupon, User, Product])],
   controllers: [CartsController],
   providers: [CartsService],
 })
